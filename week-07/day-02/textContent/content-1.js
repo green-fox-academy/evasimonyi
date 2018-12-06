1b356464674e7c989dc18248862ca11418d9e7ef
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-06 12:28:58 +0100

    feat(): DOM content1 task created

---
+++ week-07/day-02/textContent/content-1.js
@@ -0,0 +1,29 @@
+/*
+    1)  Fill every paragraph with the last one's content.
+    2)  Do the same again, but you should keep the cat strong.
+*/
+
+//1
+let paragraphs = document.querySelectorAll('p');
+const animals = document.querySelector('.animals');
+
+paragraphs.forEach(e => {
+  console.log(e.innerText);
+});
+
+paragraphs.forEach(e => {
+  e.innerText = animals.innerText;
+});
+
+paragraphs.forEach(e => {
+  console.log(e.innerText);
+});
+
+//2
+paragraphs.forEach(e => {
+  e = animals;
+});
+
+paragraphs.forEach(e => {
+  console.log(e);
+});
