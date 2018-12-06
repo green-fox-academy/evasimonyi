c17fba3d1db85edb363420ef1c7f22b52fa9164c
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-06 11:44:03 +0100

    feat(): DOM workshop queryselector1 task

---
+++ week-07/day-02/queryselector/queryselector-1.js
@@ -0,0 +1,39 @@
+/* 
+	1. store the element that says 'The King' in the 'king' variable.
+	console.log it.
+
+	2. store 'The Businessman' and 'The Lamplighter'
+	in the 'businessLamp' variable.
+	console.log each of them.
+
+	3. store 'The King' and 'The Conceited Man'
+	in the 'conceitedKing' variable.
+	alert them one by one.
+
+	4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+	in the 'noBusiness' variable.
+	console.log each of them.
+*/
+
+//1
+const king = document.querySelector('.asteroid').textContent;
+console.log(king);
+
+//2
+const businessLamp = document.querySelectorAll('.big');
+businessLamp.forEach(e => {
+  console.log(e.innerText);
+});
+
+//3
+const conceitedKing = document.querySelectorAll('.container');
+conceitedKing.forEach(e => {
+  alert(e.innerText);
+});
+
+//4
+const noBusiness = document.querySelectorAll('.container, .b329');
+noBusiness.forEach(e => {
+  console.log(e.innerText);
+});
+
