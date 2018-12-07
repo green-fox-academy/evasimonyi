0cd680ae80c2880836fed12a92d43cc4d5d81b8a
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-07 16:37:17 +0100

    gallery project update

--- week-07/day-03/slider.js
+++ week-07/day-03/slider.js
@@ -2,31 +2,30 @@ const leftButton = document.querySelector('.btnlft');
 const rightButton = document.querySelector('.btnrght');
 
 const mainImage = document.querySelector('.image');
-// console.log(mainImage.getAttribute('src'));
-// // mainImage.setAttribute('src', './2.jpg');
 
-const images = document.querySelector('img');
+const images = document.querySelectorAll('img');
 
 // leftButton.addEventListener('click', function () {
-//   // console.log('cica');
-//   // setAttribute for setting any attributes
+//   mainImage.setAttribute('src', './3.jpg');
+// });
+
+
+// rightButton.addEventListener('click', function () {
 //   mainImage.setAttribute('src', './2.jpg');
 // });
 
-leftButton.addEventListener = () => {
-  mainImage.setAttribute('src', './3.jpg');
-}
+// rightButton.addEventListener('click', function () {
+//   mainImage.setAttribute('src', './1.jpg');
+// });
+
+leftButton.addEventListener('click', function () {
+  images.forEach((e, i) => {
+    mainImage.setAttribute('src', e.getAttribute('src'));
+  });
+});
 
 rightButton.addEventListener('click', function () {
-  mainImage.setAttribute('src', './2.jpg');
-});
-
-rightButton.clickon('click', function () {
-  mainImage.setAttribute('src', './1.jpg');
-});
-
-leftButton.clickon('click', function () {
   images.forEach((e, i) => {
-    e.setAttribute('src', i.jpg)
+    mainImage.setAttribute('src', e.getAttribute('src'));
   });
 });
