78a51ca2f5e9cb8b25870aa2320acbf1afeb4fb1
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-07 15:48:42 +0100

    gallery project update commit

--- week-07/day-03/slider.js
+++ week-07/day-03/slider.js
@@ -1,6 +1,32 @@
 const leftButton = document.querySelector('.btnlft');
 const rightButton = document.querySelector('.btnrght');
 
-leftButton.addEventListener('click', function () {});
-rightButton.addEventListener('click', function () {});
+const mainImage = document.querySelector('.image');
+// console.log(mainImage.getAttribute('src'));
+// // mainImage.setAttribute('src', './2.jpg');
 
+const images = document.querySelector('img');
+
+// leftButton.addEventListener('click', function () {
+//   // console.log('cica');
+//   // setAttribute for setting any attributes
+//   mainImage.setAttribute('src', './2.jpg');
+// });
+
+leftButton.addEventListener = () => {
+  mainImage.setAttribute('src', './3.jpg');
+}
+
+rightButton.addEventListener('click', function () {
+  mainImage.setAttribute('src', './2.jpg');
+});
+
+rightButton.clickon('click', function () {
+  mainImage.setAttribute('src', './1.jpg');
+});
+
+leftButton.clickon('click', function () {
+  images.forEach((e, i) => {
+    e.setAttribute('src', i.jpg)
+  });
+});
