28dffa94a45f0bf0f2d719b81d79deaba8b79168
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-07 13:40:56 +0100

    feat(): gallery project files created, basics

---
+++ week-07/day-03/slider.js
@@ -0,0 +1,6 @@
+const leftButton = document.querySelector('.btnlft');
+const rightButton = document.querySelector('.btnrght');
+
+leftButton.addEventListener('click', function () {});
+rightButton.addEventListener('click', function () {});
+
