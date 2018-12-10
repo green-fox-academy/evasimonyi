87d766d3ecfd4763b99615e0483907677fa8f917
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-10 21:49:06 +0100

    flexbox practrice + ajax practice

--- week-08/day-01/poker/deck.js
+++ week-08/day-01/poker/deck.js
@@ -3,17 +3,19 @@ class Deck {
     this.deck = [];
     this.dealt_cards = [];
   }
+
   generate_deck() {
     let card = (suit, value) => {
-      this.name = value + ' of ' + suit;
+      this.name = value + suit;
       this.suit = suit;
       this.value = value;
 
-      return { name: this.name, suit: this.suit, value: this.value };
+      // return { name: this.name, suit: this.suit, value: this.value };
+      return this.name;
     }
 
     let values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-    let suits = ['clubs', 'diamonds', 'spades', 'hearts'];
+    let suits = ['C', 'D', 'S', 'H'];
 
     for (let i = 0; i < suits.length; i++) {
       for (let k = 0; k < values.length; k++) {
@@ -45,30 +47,35 @@ class Deck {
   }
 
   deal(num_cards) {
-
     let cards = []
-
     for (let c = 0; c < num_cards; c++) {
-      let dealt_card = this.deck.shift()
-      cards.push(dealt_card)
-      this.dealt_cards.push(dealt_card)
+      let dealt_card = this.deck.shift();
+      cards.push(dealt_card);
+      this.dealt_cards.push(dealt_card);
     }
-
-    return cards
+    return cards;
   }
 
   replace() {
-    this.deck.unshift(this.dealt_cards.shift())
+    this.deck.unshift(this.dealt_cards.shift());
   }
 
   clear_deck() {
-    this.deck = []
+    this.deck = [];
   }
 
+  sort(hand) {
+
+  }
 }
+
+module.exports = { Deck };
 
 let deck = new Deck();
 deck.generate_deck();
-deck.printDeck();
 deck.shuffle();
-deck.printDeck();
+
+let blackHand = deck.deal(5);
+let whiteHand = deck.deal(5);
+console.log(whiteHand);
+console.log(blackHand);
---
+++ week-08/day-01/poker/test.ts
@@ -0,0 +1,9 @@
+declare function require(path: string): any;
+const test = require('tape');
+import { Deck } from './deck';
+
+test ('higher hand', t => {
+  let blackHand = ['2H', '3D', '5S', '9C', 'KD'];
+  let whiteHand = ['2C', '3H', '4S', '8C', 'AH'];
+  
+})
