42cb6c90c3a83c6a9451e066ce3371bbf6ab9334
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-11 16:10:40 +0100

    new york times exercise fixed

--- week-08/day-02/nyt/nyt.js
+++ week-08/day-02/nyt/nyt.js
@@ -6,22 +6,34 @@ xhr.onreadystatechange = () => {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
-
-      const headline = document.querySelector('.headline');
-      const snippet = document.querySelector('.snippet');
-      const publicationDate = document.querySelector('.pubdate');
-
-      console.log(response.response.docs);
+      const article = document.querySelector('.article');
+      // console.log(response.response.docs);
 
       response.response.docs.forEach(e => {
-        console.log(e.snippet);
-        console.log(e.headline);
-        console.log(e.pub_date);
 
+        const div = document.createElement('div');
+        div.classList.add('cica');
+
+        const links = document.createElement('a');
+        links.setAttribute('href', e.web_url);
+        links.innerText = e.headline.main;
+        // console.log(links);
+
+        const snippet = document.createElement('div');
+        snippet.innerText = e.snippet;
+
+        const pubdate = document.createElement('div');
+        pubdate.innerText = e.pub_date;
+
+        div.appendChild(links);
+        div.appendChild(snippet);
+        div.appendChild(pubdate);
+
+        article.appendChild(div);
       })
     }
   };
 }
 
-xhr.open('GET', `${API_BASE_URL}?q=moon+landing+by+Apollo+11&page=2&sort=oldest&api-key=d0ce66e0e98e417b829cd0c22ed1f424`);
+xhr.open('GET', `${API_BASE_URL}?q=moon+landing+by+Apollo+11&page=1&sort=oldest&api-key=d0ce66e0e98e417b829cd0c22ed1f424`);
 xhr.send();
