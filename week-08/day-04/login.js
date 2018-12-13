77ed67a8b076c4618a3e54084bf54276fd5024d7
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-13 15:31:53 +0100

    form login password

---
+++ week-08/day-04/login.js
@@ -0,0 +1,12 @@
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+const password = document.querySelector('password');
+
+password.addEventListener('onmousedown', showPassword);
