da267b132a4d9b921370da723a535f84ede2c406
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-14 00:15:15 +0100

    disabled button fix

--- week-08/day-04/buttonDisable/button-disable.js
+++ week-08/day-04/buttonDisable/button-disable.js
@@ -1,15 +1,33 @@
 const buttons = document.querySelectorAll('button');
-const choices = document.querySelectorAll('.choose');
+const labels = document.querySelectorAll('label');
+let signupbutton = document.querySelector('#signupbutton');
+let lovecats = document.querySelector('#loveCats');
+const cat = document.querySelector('#cat');
+const yes = document.querySelector('#yes');
 
-buttons.forEach(e => {
-  // console.log(e);
-  e.disabled = true;
-});
+const animals = document.querySelector('#animals');
+const yesno = document.querySelector('#yesno')
 
-choices.forEach(e => {
-  console.log(e);
-  if (e.innerText === 'Dog') {
-    console.log('cica');
+animals.addEventListener('change', () => {
+  if (event.target.value === 'dog' || event.target.value === 'cat') {
+    signupbutton.removeAttribute('disabled');
   }
-});
+})
+
+yesno.addEventListener('change', () => {
+  if (event.target.value === 'yes') {
+    lovecats.removeAttribute('disabled');
+  }
+})
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (event.target.value === 'dog' || event.target.value === 'cat') {
+      alert('Thank you, you\'ve successfully signed up for cat facts');
+    } else {
+      alert('Sigh, we still added you to the cat facts list');
+    }
+  })
+  
+} )
 
