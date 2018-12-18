f1e5a51263516277f813cec4b63e08f13565377f
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-18 16:29:38 +0100

    week 9 day 1 moving folders

---
+++ week-09/day-01/greetings/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: req.query.name || 'Guest'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
