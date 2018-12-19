e3dad0cc6f2c059639d35efb20e9f1fdc00ae37b
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-19 13:47:55 +0100

    REST project app doubling, greetings

--- week-09/day-03/app.js
+++ week-09/day-03/app.js
@@ -9,6 +9,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  if (req.query.input) {
+    res.send({
+      "received": req.query.input,
+      "result": req.query.input * 2
+    })
+  } else {
+    res.send({ "error": "Please provide an input!" });
+  }
+});
+
+app.get('/greeter', (req, res) => {
+  if (req.query.name && req.query.title) {
+    res.send({
+      "welcome_message": `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+    })
+  } else if (req.query.name !== true) {
+    res.send({ "error": "Please provide a name!" });
+  } else if (req.query.title !== true) {
+    res.send({ "error": "Please provide a title!" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
