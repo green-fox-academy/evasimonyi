e2b357efc9283eec73453188672c4cc69881be93
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-28 20:33:48 +0100

    bookstore script

--- week-09/day-04/workshop_bookstore/static/script.js
+++ week-09/day-04/workshop_bookstore/static/script.js
@@ -5,7 +5,10 @@ xhr.open('GET', '/books');
 xhr.onload = () => {
   if (xhr.status === 200) {
     const res = JSON.parse(xhr.responseText);
-    console.log(res);
+    // console.log(res);
+    res.forEach((e, i) => {
+      console.log(res[i].book_name);
+    });
   }
 }
 xhr.send();
