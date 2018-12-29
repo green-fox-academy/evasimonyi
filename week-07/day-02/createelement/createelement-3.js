05d4a7ffcb5bcedb22f6ff9d7340e61b9c2bfcfe
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-29 11:02:40 +0100

    week 7 day 2 create element 3

---
+++ week-07/day-02/createelement/createelement-3.js
@@ -0,0 +1,32 @@
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+// 1) Create an <article> element for each kid
+// 2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
+//     - The H3 should contain the owner's name
+//     - The p should contain the pet's name
+// 3) Add the article to the pets div.
+
+const div = document.querySelector('div');
+
+kids.forEach(e => {
+  const article = document.createElement('article');
+  const h3 = document.createElement('h3');
+  h3.textContent = e.owner;
+  const p = document.createElement('p');
+  p.textContent = e.petName;
+  article.appendChild(h3);
+  article.appendChild(p);
+  div.appendChild(article);
+})
