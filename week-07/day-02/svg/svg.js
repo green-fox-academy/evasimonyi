710c0bf5f2aa2c19cddea007cedd1295b42e532e
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-29 11:38:58 +0100

    week 7 day 2 svg

---
+++ week-07/day-02/svg/svg.js
@@ -0,0 +1,9 @@
+// 1) Select the rectangle by the ID: "very-rectangle"
+// 2) Change the rectangle's position to x:50, y:50
+// 3) Change its fill to tomato
+
+const rectangle = document.querySelector('#very-rectangle');
+
+rectangle.style.x = '50';
+rectangle.style.y = '50';
+rectangle.style.stroke = 'tomato';
