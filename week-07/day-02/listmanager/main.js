8ec23a25c44ce0b183b15e635d5a224a94719630
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-30 10:16:40 +0100

    week 7 day 2 listmanager buttons

--- week-07/day-02/listmanager/main.js
+++ week-07/day-02/listmanager/main.js
@@ -9,6 +9,7 @@
 // 5, Check all the edge cases, no error should be printed to the console
 
 const body = document.querySelector('body');
+let selectedItemIndex = 0;
 
 //creating shopping list
 const buyItems = ['bread', 'milk', 'orange', 'tomato'];
@@ -26,16 +27,16 @@ buyItems.forEach(e => {
 const buttonsDiv = document.createElement('div');
 
 const upButton = document.createElement('button');
-upButton.setAttribute('id', 'up-button');
+upButton.setAttribute('id', 'up');
 upButton.innerHTML = 'Up'
 const downButton = document.createElement('button');
-downButton.setAttribute('id', 'down-button');
+downButton.setAttribute('id', 'down');
 downButton.innerHTML = 'Down';
 const deleteButton = document.createElement('button');
-deleteButton.setAttribute('id', 'delete-button');
+deleteButton.setAttribute('id', 'delete');
 deleteButton.innerHTML = 'X';
 const moveButton = document.createElement('button');
-moveButton.setAttribute('id', 'move-button');
+moveButton.setAttribute('id', 'move');
 moveButton.innerHTML = '>';
 
 buttonsDiv.appendChild(upButton);
@@ -50,5 +51,52 @@ const selectedItems = document.createElement('div');
 body.appendChild(selectedItems);
 
 //adding functions to buttons
+//not my solution 
+
+// --------------------------------------------------------------------
+
+const coloringItem = (shouldColor, removeColor) => {
+  shouldColor.setAttribute('style', 'background-color: grey');
+  removeColor.setAttribute('style', 'background-color: white');
+}
+
 upButton.onclick = () => {
+  if (selectedItemIndex !== 0 && document.querySelector('#shoppinglist').querySelectorAll('li').length !== 0) {
+    selectedItemIndex--;
+    let shouldColor = document.querySelector('#shoppinglist').childNodes[selectedItemIndex];
+    let removeColor = document.querySelector('#shoppinglist').childNodes[selectedItemIndex + 1];
+    coloringItem(shouldColor, removeColor);
+  }
+}
+
+downButton.onclick = () => {
+  if (selectedItemIndex !== document.querySelector('#shoppinglist').querySelectorAll('li').length - 1 && document.querySelector('#shoppinglist').querySelectorAll('li').length !== 0) {
+    selectedItemIndex++;
+    let shouldColor = document.querySelector('#shoppinglist').childNodes[selectedItemIndex];
+    let removeColor = document.querySelector('#shoppinglist').childNodes[selectedItemIndex - 1];
+    coloringItem(shouldColor, removeColor);
+  }
+}
+
+moveButton.onclick = () => {
+  if (document.querySelector('#shoppinglist').querySelectorAll('li').length >= 1) {
+    let newListElement = document.createElement('li');
+    newListElement.innerHTML = shoppinglist.childNodes[selectedItemIndex].textContent;
+    selectedItems.appendChild(newListElement);
+    shoppinglist.removeChild(shoppinglist.childNodes[selectedItemIndex]);
+  } 
+  if (document.querySelector('#shoppinglist').querySelectorAll('li').length !== 0) {
+    shoppinglist.childNodes[0].setAttribute('style', 'background-color: grey');
+    selectedItemIndex = 0;
+  }
+}
+
+deleteButton.onclick = () => {
+  if (document.querySelector('#shoppinglist').querySelectorAll('li').length >= 1) {
+    shoppinglist.removeChild(shoppinglist.childNodes[selectedItemIndex]);
+  }
+  if (document.querySelector('#shoppinglist').querySelectorAll('li').length !== 0) {
+    shoppinglist.childNodes[0].setAttribute('style', 'background-color: grey');
+    selectedItemIndex = 0;
+  }
 }
