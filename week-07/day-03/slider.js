5bcdb51f772cc502de7ddcd4a89df930337e6c9c
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2018-12-30 12:54:17 +0100

    week 7 day 3 gallery project, slide works

--- week-07/day-03/slider.js
+++ week-07/day-03/slider.js
@@ -1,30 +1,82 @@
-const leftButton = document.querySelector('.btnlft');
-const rightButton = document.querySelector('.btnrght');
+const leftButton = document.querySelector('.leftbutton');
+const rightButton = document.querySelector('.rightbutton');
+const thumbnails = document.querySelector('.thumbnails');
 
-const thumbnails = document.querySelector('.img');
+const pictures = [
+  {
+    'title': 'picture1',
+    'url': 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2018%2F03%2F21041247%2Fpuppies.jpg&w=1000&c=sc&poi=face&q=70',
+    'description': 'this is the first cute dog'
+  },
+  {
+    'title': 'picture2',
+    'url': 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12193133/German-Shepherd-Puppy-Fetch.jpg',
+    'description': 'this is the second cute dog'
+  },
+  {
+    'title': 'picture3',
+    'url': 'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+    'description': 'this is the third cute dog'
+  },
+  {
+    'title': 'picture4',
+    'url': 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/All-about-puppies--Cesar’s-tips%2C-tricks-and-advice.jpg?itok=bi9xUvwe',
+    'description': 'this is the fourth cute dog'
+  },
+  {
+    'title': 'picture5',
+    'url': 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/05/22224952/beagle-puppy-in-large-cushion-chair.jpg',
+    'description': 'this is the fifth cute dog'
+  },
+  {
+    'title': 'picture6',
+    'url': 'https://cdn1-www.dogtime.com/assets/uploads/2010/12/puppies.jpg',
+    'description': 'this is the sixth cute dog'
+  },
+];
 
-// const images = document.querySelectorAll('img');
+const mainImg = document.querySelector('.mainImg');
+mainImg.setAttribute('src', pictures[0].url);
 
-// leftButton.addEventListener('click', function () {
-//   mainImage.setAttribute('src', './3.jpg');
-// });
+for (let i = 0; i < pictures.length; i++) {
+  const thumbnail = document.createElement('img');
+  thumbnail.src = pictures[i].url;
+  thumbnails.appendChild(thumbnail);
+}
 
-// rightButton.addEventListener('click', function () {
-//   mainImage.setAttribute('src', './2.jpg');
-// });
+const images = document.querySelectorAll('img');
 
-// rightButton.addEventListener('click', function () {
-//   mainImage.setAttribute('src', './1.jpg');
-// });
+// const displayThisPic = (e) => {
+//   mainImg.setAttribute('src', e.target.url);
+// }
 
-leftButton.addEventListener('click', function () {
-  thumbnails.forEach((e, i) => {
-    mainImage.setAttribute('src', e[i].getAttribute('src'));
-  });
-});
+// images.forEach(e => {
+//   console.log(e);
+//   e.addEventListener('click', displayThisPic)
+// })
 
-rightButton.addEventListener('click', function () {
-  thumbnails.forEach(e => {
-    mainImage.setAttribute('src', e.getAttribute('src'));
-  });
-});
+let selectedPictureIndex = 0;
+
+const showNextPic = () => {
+  if (selectedPictureIndex !== pictures.length - 1) {
+    selectedPictureIndex++;
+    mainImg.setAttribute('src', pictures[selectedPictureIndex].url);
+  } else if (selectedPictureIndex === pictures.length - 1) {
+    selectedPictureIndex = 0;
+    mainImg.setAttribute('src', pictures[selectedPictureIndex].url);
+  }
+}
+
+rightButton.addEventListener("click", showNextPic);
+
+const showPreviousPic = () => {
+  if (selectedPictureIndex >= 1) {
+    selectedPictureIndex--;
+    mainImg.setAttribute('src', pictures[selectedPictureIndex].url);
+  } else if (selectedPictureIndex === 0) {
+    selectedPictureIndex = pictures.length;
+    mainImg.setAttribute('src', pictures[selectedPictureIndex].url);
+  }
+}
+
+leftButton.addEventListener("click", showPreviousPic);
