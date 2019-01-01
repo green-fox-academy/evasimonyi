1a8a5aa9ee058f078518f7faa852ee004fb0033a
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-01-01 18:24:51 +0100

    week 8 day 4 disable button fix

--- week-08/day-04/buttonDisable/button-disable.js
+++ week-08/day-04/buttonDisable/button-disable.js
@@ -1,32 +1,51 @@
+'use strict';
+
+//buttons
 const buttons = document.querySelectorAll('button');
-const labels = document.querySelectorAll('label');
 let signupbutton = document.querySelector('#signupbutton');
 let lovecats = document.querySelector('#loveCats');
-const cat = document.querySelector('#cat');
-const yes = document.querySelector('#yes');
 
+// questions
 const animals = document.querySelector('#animals');
-const yesno = document.querySelector('#yesno')
+const yesno = document.querySelector('#yesno');
 
+let lovesCats = false;
+let goldfishlover = true;
+
+// If you choose Dog or Cat, it should enable the sign up button
 animals.addEventListener('change', () => {
-  if (event.target.value === 'dog' || event.target.value === 'cat') {
+  if (event.target.id === 'dog' || event.target.id === 'cat') {
+    goldfishlover = false;
     signupbutton.removeAttribute('disabled');
+  } else if (event.target.value === 'goldfish') {
+    goldfishlover === true;
   }
 })
 
+// If you choose Yes for cat facts, it should enable the I love cats button
+// If you pick Victor and no to cat facts it should enable the sign up button only
 yesno.addEventListener('change', () => {
   if (event.target.value === 'yes') {
     lovecats.removeAttribute('disabled');
+    lovesCats === true;
+  } else if (goldfishlover && event.target.value === 'no') {
+    signupbutton.removeAttribute('disabled');
+    lovesCats === false;
   }
 })
 
+// Clicking on a button should alert:
+// Thank you, you've successfully signed up for cat facts
+// Cilcking this time should display: Sigh, we still added you to the cat facts list
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    if (event.target.value === 'dog' || event.target.value === 'cat') {
-      alert('Thank you, you\'ve successfully signed up for cat facts');
-    } else {
+    if (goldfishlover && lovesCats === false) {
       alert('Sigh, we still added you to the cat facts list');
-    }
-  })
+    } else {
+      alert('Thank you, you\'ve successfully signed up for cat facts');
+    };
+  });
 })
+
 
