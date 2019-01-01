e92b1ab3ee0808dfd04c40b046c22534d873241a
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-01-01 15:26:51 +0100

    week 8 day 3 infinite scroll

---
+++ week-08/day-03/infiniteScroll/infinite-scroll.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const scrollThreshold = 300;
+let randomNumber = Math.floor((Math.random() * 100) + 1);
+
+let randomColour = () => {
+  let newColour = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+  return newColour;
+}
+
+let createBoxes = () => {
+  for (let i = 1; i <= randomNumber; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('style', `background-color: ${randomColour()}`)
+    container.appendChild(div);
+  };
+}
+
+createBoxes();
+
+let createTenBoxes = () => {
+  for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('style', `background-color: ${randomColour()}`)
+    container.appendChild(div);
+  };
+}
+
+window.onscroll = () => {
+  if (window.scrollY + window.innerHeight >= container.clientHeight - scrollThreshold) {
+    createTenBoxes();
+  };
+}
