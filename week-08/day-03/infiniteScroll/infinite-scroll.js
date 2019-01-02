4bf0c5d29c13e68d4c5c16c8be6fb2259775d6bf
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-01-02 21:24:35 +0100

    week 7 day 3 infinite scroll

--- week-08/day-03/infiniteScroll/infinite-scroll.js
+++ week-08/day-03/infiniteScroll/infinite-scroll.js
@@ -20,7 +20,7 @@ let createBoxes = () => {
 
 createBoxes();
 
-const createTenBoxes = () => {
+let createTenBoxes = () => {
   for (let i = 0; i < 10; i++) {
     const div = document.createElement('div');
     div.setAttribute('style', `background-color: ${randomColour()}`)
