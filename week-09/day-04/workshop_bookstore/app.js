23647299aaf9618f0c1896fc0e238254ee4d168c
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-01-07 10:42:47 +0100

    refactor() deleted unnecessary code from bookstore app js

--- week-09/day-04/workshop_bookstore/app.js
+++ week-09/day-04/workshop_bookstore/app.js
@@ -46,7 +46,31 @@ app.get('/books', (req, res) => {
   });
 });
 
-// con.end(err => {
+// List the following data:
+// book title
+// authors name
+// category
+// publishers name
+// price
+// Return HTML that contains the list as a <table>
+
+app.get('/bookinfo', (req, res) => {
+  const getAllBooks = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM ((book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id) INNER JOIN category ON book_mast.cate_id = category.cate_id) INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;';
+
+  conn.query(bookNames, authorsNames, category, publisherNames, price, 
+    (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).json({
+        error: 'Internal server error'
+      });
+      return;
+    }
+    res.json(data);
+  });
+});
+
+// conn.end(err => {
 //   if (err) {
 //     console.log(err);
 //     return;
