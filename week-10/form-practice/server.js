6a0b5dc5e7be8937a9bb1e287b5dfd75578e9195
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-01-09 14:26:02 +0100

    fix(): submit all answers at once

--- week-10/form-practice/server.js
+++ week-10/form-practice/server.js
@@ -1,11 +1,9 @@
 'use strict';
-require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const path = require('path');
 const PORT = 3000;
-
 
 app.use('/assets', express.static('assets'));
 app.use(express.json());
@@ -14,14 +12,12 @@ app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); }
 
 app.post('/', (req, res) => {
   const { checkedArr } = req.body;
-  console.log(checkedArr)
   res.json({
     message: checkedArr,
   });
 });
 
 app.post('/favanimal', (req, res) => {
-  // const { pickedAnswer} = req.body;
   if (req.body.pickedAnswer) {
     res.json({
       message: req.body.pickedAnswer,
@@ -37,16 +33,12 @@ app.post('/dropdown', (req, res) => {
   }
 });
 
-app.put('/bla', (req, res) => {
-  conn.query(`UPDATE table SET frontgarden = ${req.body.frontgarden}, backgarden =${req.body.backgarden}, garage WHERE id = 2`, (err, data) => {
-    if (err) {
-      res.json({
-        error: err
-      })
-    }
+app.post('/final', (req, res) => {
+  if (req.body.pickedAnswer && req.body.checkedArr && req.body.pickedAnimal) {
+    res.json({
+      message: req.body,
+    });
   }
-  )
-  res.json({ asd: 'dfgh' })
-})
+});
 
 app.listen(PORT, () => { console.log(`App is listening on port: ${PORT}`); });
