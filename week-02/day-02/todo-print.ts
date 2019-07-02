e5e2849bfca49d67fbeaf6ff6cf7f3d1572eb1c4
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-02 12:36:03 +0200

    fix todo print with concat

--- week-02/day-02/todo-print.ts
+++ week-02/day-02/todo-print.ts
@@ -1,15 +1,14 @@
+'use strict';
 // Add "My todo:" to the beginning of the todoText
 // Add " - Download games" to the end of the todoText
 // Add " - Diablo" to the end of the todoText but with indention
+
 // Expected outpt:
+
 // My todo:
 //  - Buy milk
 //  - Download games
 //      - Diablo
 
-'use strict';
-export {};
-
-let todoText: string [] = ['My Todo:', ' - Buy milk\n', '- Download games','- Diablo'];
-
-console.log(todoText);
+let todoText: string = ' - Buy milk\n';
+console.log("My todo:".concat('\n', todoText, " - Download games\n").concat("\t - Diablo"));
