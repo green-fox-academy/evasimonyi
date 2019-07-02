c53c2fae3dbf0a178ce9bfa1499f52a74a778e4f
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-02 14:01:06 +0200

    fix list intro with emptying array

--- week-02/day-02/listintro1.ts
+++ week-02/day-02/listintro1.ts
@@ -25,12 +25,27 @@ for (let i: number = 0; i < names.length; i++) {
 names.splice(1, 1);
 console.log(names);
 
-
-// elso megoldas
+// 1 reverse
 let reversed = names.reverse();
 console.log(reversed);
 
-// masodik megoldas
+// 2 reverse
 for (let i = 0; i < reversed.length; i++) {
   console.log(reversed[i]);
 }
+
+// solution1 delete every element 
+names = [];
+console.log(names);
+
+// solution2 delete every element 
+// to check this, comment out the first solution
+names.splice(0, names.length);
+console.log(names);
+
+// solution3 delete every element
+// to check this, comment out the first two solutions
+while (names.length > 0) {
+  names.pop();
+}
+console.log(names);
