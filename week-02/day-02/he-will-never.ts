d341d367411ca820646c7d7ed986c2fe3412ac30
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-03 08:59:07 +0200

    fix hewillnever with type

--- week-02/day-02/he-will-never.ts
+++ week-02/day-02/he-will-never.ts
@@ -1,5 +1,5 @@
 'use strict';
-export {};
+export { };
 
 // Things are a little bit messed up
 // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
@@ -8,7 +8,7 @@ export {};
 let out: string = '';
 let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
 
-let hashmap = {
+let hashmap: any = {
   7: 'run around and desert you',
   50: 'tell a lie and hurt you ',
   49: 'make you cry, ',
@@ -19,7 +19,7 @@ let hashmap = {
   3: 'say goodbye '
 };
 
-for (let i : number = 0; i< notSoCrypticMessage.length; i++){
+for (let i: number = 0; i < notSoCrypticMessage.length; i++) {
   out = out + hashmap[notSoCrypticMessage[i]];
 }
 
