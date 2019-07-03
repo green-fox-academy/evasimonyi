882981cab1bfc84538cd5a57ceabf273d222fa54
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-03 12:23:34 +0200

    fix rainbow

--- week-02/day-03/rainbowbox/rainbow-box-function-ts.ts
+++ week-02/day-03/rainbowbox/rainbow-box-function-ts.ts
@@ -1,34 +1,34 @@
 'use strict';
+export = {};
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 // Create a square drawing function that takes 2 parameters:
 // The square size, and the fill color,
 // and draws a square of that size and color to the center of the canvas.
 // Create a loop that fills the canvas with rainbow colored squares.
- 
-let size : number = 30;
+
+let size: number = 30;
 let colour = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 let colourIndex = 0;
+canvas.width = 600;
+canvas.height = 400;
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height
+let x = (canvasWidth / 2) - (size / 2);
+let y = (canvasHeight / 2) - (size / 2);
 
-function draw (size,colour){
-    let x = (canvas.width / 2) - (size / 2);
-    let y = (canvas.height / 2) - (size / 2);
-    ctx.fillStyle = colour [colourIndex];
-    ctx.fillRect(x, y, size, size);
-    colourIndex = colourIndex + 1 ;
+function draw(size: number, colourArray: string[]) {
+  ctx.fillStyle = colourArray[colourIndex];
+  ctx.fillRect(x, y, size, size);
 };
 
-for (let i : number = 0; i < size; i++) {
-    draw(size, colour);
-    x = x + size;
-    y = y + size;
+for (let i: number = 0; i < size; i++) {
+  draw(size, colour);
+  colourIndex = colourIndex + 1;
+  x += size;
+  y += size;
+  size = + size;
 };
-
-/* for (let k : number = 0; k < size; i++) {
-    draw(size, colour);
-    x = x + size;
-    y = y + size;
-}; */
