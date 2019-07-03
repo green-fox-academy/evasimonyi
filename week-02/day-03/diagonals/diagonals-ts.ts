36620c9c3d8fc9057d6fd6333c2961ce7a93ec66
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-03 15:23:09 +0200

    fix diagonals with function

--- week-02/day-03/diagonals/diagonals-ts.ts
+++ week-02/day-03/diagonals/diagonals-ts.ts
@@ -1,7 +1,8 @@
 'use strict';
+export = {};
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 // Draw the canvas' diagonals.
@@ -9,14 +10,37 @@ const ctx = canvas.getContext('2d');
 
 // Draw a colored line
 
-ctx.strokeStyle = 'green';
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(600, 400);
-ctx.stroke();
+// function nelkul, jo megoldas lasd lejjebb
+// ctx.strokeStyle = 'green';
+// ctx.beginPath();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(600, 400);
+// ctx.stroke();
 
-ctx.strokeStyle = 'red';
-ctx.beginPath();
-ctx.moveTo(600, 0);
-ctx.lineTo(0, 400);
-ctx.stroke(); 
+// ctx.strokeStyle = 'red';
+// ctx.beginPath();
+// ctx.moveTo(600, 0);
+// ctx.lineTo(0, 400);
+// ctx.stroke();
+
+canvas.width = 600;
+canvas.height = 400;
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height
+
+function drawDiagonal(x: number, y: number): any {
+  ctx.beginPath();
+  if (x === 0 && y === 0) {
+    ctx.strokeStyle = 'green';
+    ctx.moveTo(x, y);
+    ctx.lineTo(canvasWidth, canvasHeight);
+  } else {
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(canvasWidth, 0);
+    ctx.lineTo(0, canvasHeight);
+  }
+  ctx.stroke();
+}
+
+drawDiagonal(0, 0);
+drawDiagonal(canvasWidth, canvasHeight);
