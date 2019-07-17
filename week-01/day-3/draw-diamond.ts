d2987c3053015d239ce423f64f87f631b7c7982b
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-17 22:14:39 +0200

    chore(diamond): Refactor

--- week-01/day-3/draw-diamond.ts
+++ week-01/day-3/draw-diamond.ts
@@ -1,6 +1,5 @@
 'use strict';
-
-export {};
+export { };
 
 // Write a program that draws a
 // diamond like this:
@@ -13,46 +12,16 @@ export {};
 //   ***
 //    *
 //
-// The diamond should have as many lines as lineCount is
-
-/*      3 space, 1 star
-        2 space, 3 star
-        1 space, 5 star
-        0 space, 7 star */
 
 let lineCount: number = 7;
-let line: string = '*';
+let star: string = '*';
 let space: string = '';
 
-for (let j = 0; j <= lineCount / 2; j++){ 
-        space = '';
-        for (let i = 0; i < lineCount - line.length / 2 ; i++) {
-                space += ' ';
-        };
-        console.log(space, line);
-        line = '*' + line + '*';
-        
+for (let j = 0; j <= lineCount / 2 + 1; j++) {
+	space = '';
+	for (let i = 0; i < lineCount - star.length / 2; i++) {
+		space += '1';
+	};
+	console.log(space, star);
+	star = '*' + star + '*';
 };
-
-/*      1 space, 5 star
-        2 space, 3 star
-        3 space, 1 star */
-        
-let sp: string = ' ';
-let repeatTimes= lineCount;
-// fill the k variable with its content 4 times
-        
-function repeat(sp) {
-for (let l = 0; l < repeatTimes; l++) {
-        console.log(sp);
-            }
-        }
-repeat(sp);
-
-for (let k = 0; k <= lineCount / 2; k--){
-        space = ' ';
-        for (let b = lineCount; b > 0; b--)
-                console.log(space, line);
-                line = space + line + space;
-
-}
