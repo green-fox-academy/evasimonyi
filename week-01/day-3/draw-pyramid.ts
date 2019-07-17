7b5f5c9a116c35875ba26340d4eb0bf1d6154fa0
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-07-17 22:14:13 +0200

    fix(pyramid): Fix drawing pyramid

--- week-01/day-3/draw-pyramid.ts
+++ week-01/day-3/draw-pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-export {};
+export { };
 
 // Write a program that draws a
 // pyramid like this:
@@ -16,17 +16,11 @@ let lineCount: number = 4;
 let line: string = '*';
 let space: string = '';
 
-for (let j = 1; j <= lineCount; j++){ 
-        space = '';
-        for (let i = 0; i < lineCount - line.length / 2 ; i++) {
-                space += ' ';
-        };
-        console.log(space, line);
-        line = '*' + line + '*';
-        
+for (let j = 1; j <= lineCount; j++) {
+  space = '';
+  for (let i = 0; i < lineCount - line.length / 2; i++) {
+    space += ' ';
+  };
+  console.log(space, line);
+  line = '*' + line + '*';
 };
-
-/*      3 space, 1 star
-        2 space, 3 star
-        1 space, 5 star
-        0 space, 7 star */
