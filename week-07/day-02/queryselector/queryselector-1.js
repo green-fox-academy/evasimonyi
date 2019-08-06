1e76bace31b3ee39881d03434c34f9c0091545ce
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-06 13:21:31 +0200

    chore(DOM): refactoring

--- week-07/day-02/queryselector/queryselector-1.js
+++ week-07/day-02/queryselector/queryselector-1.js
@@ -1,39 +1,16 @@
-/* 
-	1. store the element that says 'The King' in the 'king' variable.
-	console.log it.
-
-	2. store 'The Businessman' and 'The Lamplighter'
-	in the 'businessLamp' variable.
-	console.log each of them.
-
-	3. store 'The King' and 'The Conceited Man'
-	in the 'conceitedKing' variable.
-	alert them one by one.
-
-	4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
-	in the 'noBusiness' variable.
-	console.log each of them.
-*/
-
-//1
-const king = document.querySelector('.asteroid').textContent;
+// 1. store the element that says 'The King' in the 'king' variable and console.log it.
+const king = document.querySelector('#b325').textContent;
 console.log(king);
 
-//2
+// 2. store 'The Businessman' and 'The Lamplighter' in the 'businessLamp' variable. console.log each of them.
 const businessLamp = document.querySelectorAll('.big');
-businessLamp.forEach(e => {
-  console.log(e.innerText);
-});
+businessLamp.forEach(e => console.log(e.textContent));
 
-//3
-const conceitedKing = document.querySelectorAll('.container');
-conceitedKing.forEach(e => {
-  alert(e.innerText);
-});
+// 3. store 'The King' and 'The Conceited Man' in the 'conceitedKing' variable, alert them one by one.
+const conceitedKing = document.querySelectorAll('.container > div');
+conceitedKing.forEach(e => alert(e.textContent))
 
-//4
+// 4 store 'The King', 'The Conceited Man' and 'The Lamplighter' in the 'noBusiness' variable, console.log each of them.
 const noBusiness = document.querySelectorAll('div');
-noBusiness.forEach(e => {
-  console.log(e.innerText);
-});
+noBusiness.forEach(e => console.log(e.textContent));
 
