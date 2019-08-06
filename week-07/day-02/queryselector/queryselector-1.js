bd84f1abc0dd8fb3397e938c484d7a81e6b7ed6e
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-06 13:30:10 +0200

    chore(DOM): refactoring

--- week-07/day-02/queryselector/queryselector-1.js
+++ week-07/day-02/queryselector/queryselector-1.js
@@ -1,6 +1,9 @@
 // 1. store the element that says 'The King' in the 'king' variable and console.log it.
-const king = document.querySelector('#b325').textContent;
-console.log(king);
+const king = document.querySelector('#b325');
+
+console.log(king.innerHTML);
+console.log(king.textContent);
+console.log(king.innerText);
 
 // 2. store 'The Businessman' and 'The Lamplighter' in the 'businessLamp' variable. console.log each of them.
 const businessLamp = document.querySelectorAll('.big');
@@ -13,4 +16,3 @@ conceitedKing.forEach(e => alert(e.textContent))
 // 4 store 'The King', 'The Conceited Man' and 'The Lamplighter' in the 'noBusiness' variable, console.log each of them.
 const noBusiness = document.querySelectorAll('div');
 noBusiness.forEach(e => console.log(e.textContent));
-
