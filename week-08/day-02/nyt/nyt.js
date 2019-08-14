452f3162491e34fdda17c05357e4b798677e14e1
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-14 12:08:56 +0200

    refactor(XHR): Refactor nyt: HMTL tags, CSS

--- week-08/day-02/nyt/nyt.js
+++ week-08/day-02/nyt/nyt.js
@@ -4,31 +4,24 @@ const API_BASE_URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
 let API_KEY = "?q=moon+landing+by+Apollo+11&page=1&sort=oldest&api-key=d0ce66e0e98e417b829cd0c22ed1f424"
 
 const xhr = new XMLHttpRequest();
-const container = document.querySelector('.container');
+const main = document.querySelector('main');
 
 xhr.onload = () => {
   if (xhr.status === 200) {
     const response = JSON.parse(xhr.responseText);
     response.response.docs.forEach(e => {
-      //minden 'visszakapott' cikknek letrehozok egy divet
-      const div = document.createElement('div');
-      div.classList.add('article');
-      //a cikk cime, hiperhivatkozassal, rakattintva behozza az eredeti cikket
+      const article = document.createElement('article');
       const links = document.createElement('a');
       links.setAttribute('href', e.web_url);
       links.innerText = e.headline.main;
-      //a cim ala jon a snippet, aminek letrehozok egy divet
       const snippet = document.createElement('div');
       snippet.innerText = e.snippet;
-      //datum legalulra, konvertalva
       const pubdate = document.createElement('div');
       pubdate.innerText = new Date(e.pub_date).toDateString();
-      //mindet beleteszem a divbe
-      div.appendChild(links);
-      div.appendChild(snippet);
-      div.appendChild(pubdate);
-
-      container.appendChild(div);
+      article.appendChild(links);
+      article.appendChild(snippet);
+      article.appendChild(pubdate);
+      main.appendChild(article);
     });
   };
 }
