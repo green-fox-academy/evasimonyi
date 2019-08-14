8cb93c022610bcc1c0b9e9914e3a0bb96bd54da2
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-14 12:17:10 +0200

    refactor(XHR): Refactor giphy task

--- week-08/day-02/giphy/giphy.js
+++ week-08/day-02/giphy/giphy.js
@@ -3,47 +3,22 @@
 const API_BASE_URL = "https://api.giphy.com/v1/gifs/search?api_key=9TvdL2dZ2ceVcOiZALYzBbps2OuTXJ0d&"
 let API_KEY = "q=dog&limit=16&offset=0&rating=G&lang=en"
 
-//api kulcsot kulon kell kimenteni, mint a link
-
 const xhr = new XMLHttpRequest();
 const thumbnails = document.querySelector('.thumbnails');
 
-// 1. easier, shorter
-// xhr.onload = () => {
-//   if (xhr.status === 200) {}
-
-// 2. same thing as 1. but longer
-// xhr.onreadystatechange = () => {
-//   if (xhr.readyState === XMLHttpRequest.DONE) {
-//     if (xhr.status === 200) {}
-
-// xhr.onreadystatechange = () => {
-//   if (xhr.readyState === XMLHttpRequest.DONE) {
-//     if (xhr.status === 200) {
-//       const response = JSON.parse(xhr.responseText);
-//       response.data.forEach(e => {
-//         const images = document.createElement('img');
-//         images.setAttribute('src', e.images.downsized_still.url)
-//         thumbnails.appendChild(images);
-//         images.onclick = () => {
-//           images.setAttribute('src', e.images.downsized.url);
-//         }
-//       });
-//     }
-//   }
-// };
-
 xhr.onload = () => {
-  if (xhr.status === 200) {
-    const response = JSON.parse(xhr.responseText);
-    response.data.forEach(e => {
-      const images = document.createElement('img');
-      images.setAttribute('src', e.images.downsized_still.url);
-      thumbnails.appendChild(images);
-      images.onclick = () => {
-        images.setAttribute('src', e.images.downsized.url);
-      }
-    });
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      response.data.forEach(e => {
+        const images = document.createElement('img');
+        images.setAttribute('src', e.images.downsized_still.url);
+        thumbnails.appendChild(images);
+        images.onclick = () => {
+          images.setAttribute('src', e.images.downsized.url);
+        }
+      });
+    }
   }
 }
 
