cd4087eada55c2ac938e0821f65859dad55ecbc3
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-22 12:03:42 +0200

    refactor(events): Refactor keyup task

--- week-08/day-03/keyup/keyup.js
+++ week-08/day-03/keyup/keyup.js
@@ -1,12 +1,8 @@
-// 1) Subscibe to keyup events on the global window object
-// 2) Console log the event object and peek inside
-// 3) Display the last pressed key's code as "Last pressed key code is: __"
+'use strict';
 
 const h1 = document.querySelector('h1');
-
 const body = document.querySelector('body');
 
 body.addEventListener('keyup', (event) => {
-  h1.innerText = `Last pressed key code is: ${event.keyCode} `;
+  h1.innerText = `Last pressed key code is: ${event.keyCode}`;
 });
-
