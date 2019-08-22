9c3173de78294bbe20307ebb621d8b5d3930594a
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-08-22 12:41:19 +0200

    refactor(events): Refactor click three times task, add eventlistener instead of onclick

--- week-08/day-03/click-three-times/click-three-times.js
+++ week-08/day-03/click-three-times/click-three-times.js
@@ -1,25 +1,19 @@
-// Create a simple HTML document with one button.
-// If the user clicks the button 3 times, and 5 seconds is already elapsed since the page is loaded, a text should appear under the button:
-// 5 seconds elapsed and clicked 3 times
-// If the user starts clicking before the first 5 seconds, nothing should be printed
-
 'use strict';
 
 const button = document.querySelector('button');
-// const body = document.querySelector('body');
 
 const displayText = () => {
-  const text = document.createElement('p');
+  const text = document.createElement('span');
   text.textContent = '5 seconds elapsed and clicked 3 times';
   document.body.appendChild(text);
 }
 
 window.onload = () => {
   setTimeout(() => {
-    button.onclick = (event) => {
+    button.addEventListener('click', (event) => {
       if (event.detail === 3) {
         displayText();
       };
-    }
+    })
   }, 5000);
 }
