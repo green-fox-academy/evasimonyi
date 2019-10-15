fc507c6fbcd9edd7120230f6b8915cec4d4b920e
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2019-10-15 16:18:43 +0800

    fix(week-02/day-02): Fix replace task

--- week-02/day-02/simple-replace.ts
+++ week-02/day-02/simple-replace.ts
@@ -5,6 +5,12 @@ export { };
 // Please fix it for me!
 // Expected ouput: In a galaxy far far away
 
+// this way if there is multiple 'diswasher'-s, all of them gets replaced
+let x: any = /dishwasher/gi;
+let firstExample: string = 'In a dishwasher far far away dishwasher';
+console.log(firstExample.replace(x, 'galaxy'));
+
+// this way if there is multiple 'diswasher'-s, only the first occurence gets replaced
 let dishwasher = 'dishwasher';
-let example: string = 'In a dishwasher far far away';
-console.log(example.replace(dishwasher, 'galaxy'));
+let secondExample: string = 'In a dishwasher far far away dishwasher';
+console.log(secondExample.replace(dishwasher, 'galaxy'));
