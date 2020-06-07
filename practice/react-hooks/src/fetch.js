12c1660dfa5091e1438a20d9932c79e529a17ed3
Eva Simonyi <eva.simonyi@greenfoxacademy.com>
2020-06-07 20:27:53 +0200

    delete(practice): delete unnecessary folder

--- practice/react-hooks/src/fetch.js
+++
@@ -1,6 +0,0 @@
-export default function fetchArticles(url, callback) {
-  return fetch(url)
-    .then(res => res.json())
-    .then(callback)
-    .catch(error => console.log(error))
-}
--- practice/routes-practice/src/pages/AboutPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function AboutPage() {
-  return (
-    <p>Some stuff about me</p>
-  )
-}
